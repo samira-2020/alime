32ae8c56f2e4abaf0c636d80bc45fecf9fc21bc8
samira-2020 <75680720+samira-2020@users.noreply.github.com>
2025-11-09 14:35:47 +0330

    Add files via upload

---
+++ alime/script.js
@@ -0,0 +1,43 @@
+document.getElementById('parag').style.fontSize='60px';
+var x = 'mohsen';
+console.log(x.length);
+const age =18;
+if (age>=18){
+    console.log("ali is adult");
+}else{
+    console.log("ali is child");
+}
+var f=1;
+while(f<=10){
+    console.log("samira");
+    f++;
+}
+var i=1;
+while(i<=10 ){
+    if(i%3==0){
+        console.log(i);
+    }
+    
+    i++;
+}
+var myarray=[5,4,3,2,1];
+var array2=[3,6,4,8,1];
+var array3=[7,5,3,4]
+// var ave=0;
+// for(var i=0;i<andis.length;i++){
+//     ave=andis[i]+ave;
+// }
+// console.log(ave);
+console.log( myarray);
+
+function add(num1,num2){
+    console.log(num1+num2); 
+}
+var x =add(5,6);
+console.log(x);
+// var num1=window.prompt("Please enter num1");
+
+
+// var num2 =window.prompt(num2);
+// sum=num1*num2
+// console.log(sum);
